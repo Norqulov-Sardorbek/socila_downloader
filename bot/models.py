fbcff654773d638b0f83e43d46e3a8a9d2e1f47b
Jaloliddin <104080313+Jaloliddin-Zokirov@users.noreply.github.com>
2025-07-15 11:26:32 +0500

    change

--- bot/models.py
+++ bot/models.py
@@ -5,15 +5,17 @@ from django.db import models
 
 
 class User(models.Model):
-    tg_id = models.BigIntegerField()
+    tg_id = models.BigIntegerField(unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
     
 class ChannelsToSubscribe(models.Model):
     link = models.CharField(max_length=255)
     name= models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return self.link
+        return f"User {self.tg_id}"
 
     class Meta:
+        db_table = 'bot_user'  
         verbose_name = "Channel to Subscribe"
         verbose_name_plural = "Channels to Subscribe"
